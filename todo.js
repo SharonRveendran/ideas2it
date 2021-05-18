0f4add3d7a69ec9f2807d3d38e3ba9cb0c5a8fea
Sharon <sharon.raveendran@ideas2it.com>
2021-05-18 23:11:43 +0530

    ToDo left and center part completed

--- todo.js
+++ todo.js
@@ -1,83 +1,130 @@
 (function() { 
-    let categoryId = 6;
+    let id = 6;
     let list = [
         {
             id : "category1",
             icon : "fa fa-sun",
             name : "My Day",
+            //taskList : []
         },
         {
             id : "category2",
             icon : "fa fa-star",
             name : "Important",
-            
+            //taskList : []
         },
         {
             id : "category3",
             icon : "fa fa-calendar",
-            name : 'Planned'
+            name : 'Planned',
+            //taskList : []
         },
         {
             id : "category4",
             icon : "fa fa-user",
-            name : "Assign to you"
+            name : "Assign to you",
+            //taskList : []
         },
         {
             id : "category5",
             icon : "fa fa-home",
-            name : "Task"
+            name : "Task",
+            //taskList : []
         }
     ];
-    //let myDayTaskList[];
-    let categoryList = document.getElementById('category-list');
+    let taskList = [];
+    let categoryId;
+    let categoryListBlock = document.getElementById('category-list');
     let categoryInput = document.getElementById('add-category');
     let categoryContainer = document.getElementById('category');
     let taskTitleblock = document.getElementsByClassName('title')[0];
+    let taskInput = document.getElementsByClassName('task-input')[0];
     function init() { 
         document.getElementsByClassName("center-container")[0].style.visibility = 'hidden';
-        renderCategoryList();
+        categoryList();
         categoryInput.addEventListener('keypress', function(event){ 
             addCategory(event);
         });
         categoryContainer.addEventListener("click", function(event){
             if ("SPAN" === event.target.tagName) {
-               renderTask(event);
+               renderTaskContainer(event);
             }
         });
+        taskInput.addEventListener("keypress", function(event) {
+            if (event.key === 'Enter' && taskInput.value != '') {
+                addTask();
+            } 
+        });
     }
-    function renderTask(event) {
-        document.getElementsByClassName("center-container")[0].style.visibility = 'visible';
-        taskTitleblock.innerHTML =  document.getElementById(event.target.id).innerHTML;
-    }
-    function renderCategoryList() {
+    function categoryList() {
         for (let i = 0; i < list.length; i++) {
-            const category = document.createElement('li');
-            const icon = document.createElement('i');
-            icon.setAttribute("class", list[i].icon);
-            const span = document.createElement("span");
-            span.setAttribute("id", list[i].id);
-            span.appendChild(document.createTextNode(list[i].name));
-            category.appendChild(icon);
-            category.appendChild(span);
-            categoryList.appendChild(category);
+            renderCategoryList(list[i]);
         }
     }
+    function renderCategoryList(value) {
+        const category = document.createElement('li');
+        const icon = document.createElement('i');
+        icon.setAttribute("class", value.icon);
+        const span = document.createElement("span");
+        span.setAttribute("id", value.id);
+        span.appendChild(document.createTextNode(value.name));
+        category.appendChild(icon);
+        category.appendChild(span);
+        categoryListBlock.appendChild(category);
+    }
     function addCategory(event) {
-            if (event.key == 'Enter') {
-                if (categoryInput.value == '') {
-                    categoryInput.value = 'Untitled list';
-                }
-                var category = document.createElement('li');
-                const icon = document.createElement('i');
-                icon.setAttribute("class","fa fa-list");
-                const span = document.createElement("span");
-                span.setAttribute("id", "category" + categoryId++);
-                span.appendChild(document.createTextNode(categoryInput.value));
-                category.appendChild(icon);
-                category.appendChild(span);
-                categoryList.appendChild(category);
-                categoryInput.value = '';
-            }  
+        if (event.key == 'Enter') {
+            if (categoryInput.value == '') {
+                categoryInput.value = 'Untitled list';
+            }
+            let obj = {
+                id: "category" + id++,
+                icon: 'fa fa-list',
+                name: categoryInput.value,
+                //taskList : []
+            }
+            list.push(obj);
+            renderCategoryList(obj);
+            categoryInput.value = '';
+        }  
+    }
+    function renderTaskContainer(event) {
+        const taskUl = document.getElementsByClassName('task-ul')[0];
+        categoryId = event.target.id;
+        document.getElementsByClassName("center-container")[0].style.visibility = 'visible';
+        taskTitleblock.innerHTML =  document.getElementById(categoryId).innerHTML;
+        while (taskUl.firstChild) {
+            taskUl.removeChild(taskUl.firstChild);
+        }
+        for (let i = 0; i < taskList.length; i++) {
+            if (categoryId == taskList[i].id) {
+                renderTask(taskList[i]);
+            }
+        }
+    }
+    function addTask() {
+        let newTask = {
+            id : categoryId,
+            name : taskInput.value
+        }
+        taskInput.value = '';
+        taskList.push(newTask);
+        renderTask(newTask);
+    }
+    function renderTask(newTask) {
+        const taskUl = document.getElementsByClassName('task-ul')[0];
+        const taskLi = document.createElement("li");
+        const taskCheckBox = document.createElement("input");
+        const taskSpan = document.createElement("span");
+        const icon = document.createElement("i");
+        taskCheckBox.setAttribute("type", "checkbox");
+        taskLi.appendChild(taskCheckBox);
+        taskSpan.appendChild(document.createTextNode(newTask.name));
+        taskLi.appendChild(taskSpan);
+        icon.className = "far fa-star";
+        taskLi.appendChild(icon);
+        taskUl.appendChild(taskLi);
+        document.getElementsByClassName("tasks")[0].appendChild(taskUl);
     }
     init();
 })();
