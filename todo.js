d58dfc45a12398875a8549d1c24f9342be0287f2
Sharon <sharon.raveendran@ideas2it.com>
2021-05-20 20:46:38 +0530

    todo completed

--- todo.js
+++ todo.js
@@ -1,42 +1,37 @@
 (function() { 
-    let cid = 6;
-    let tid = 0;
-    subTaskId = 0;
     let list = [
         {
             id : "category1",
             icon : "fa fa-sun",
             name : "My Day",
-            //taskList : []
         },
         {
             id : "category2",
             icon : "far fa-star",
             name : "Important",
-            //taskList : []
         },
         {
             id : "category3",
             icon : "far fa-calendar-alt",
             name : 'Planned',
-            //taskList : []
         },
         {
             id : "category4",
             icon : "far fa-user",
             name : "Assign to you",
-            //taskList : []
         },
         {
             id : "category5",
             icon : "fa fa-home",
             name : "Task",
-            //taskList : []
         }
     ];
-    let taskList = [];
-    let subTaskList = [];
-    let categoryId;
+    let cid = 6; //id for new catogery
+    let tid = 0;  //id for tsask
+    subTaskId = 0; //id for subtask
+    let taskList = []; //list for all tasks
+    let subTaskList = []; //list for all subtasks
+    let categoryId; //id for current category
     let subTaskTitle = document.getElementsByClassName('sub-task-title')[0];
     let centerContainer = document.getElementsByClassName('center-container')[0];
     let categoryListBlock = document.getElementById('category-list');
@@ -47,44 +42,39 @@
     let taskUl = document.getElementsByClassName("task-ul")[0];
     let rightContainer = document.getElementsByClassName("right-container")[0];
     let stepInput = document.getElementsByClassName('step-input')[0];
-    let stepList = document.getElementsByClassName('step-list')[0]; 
+    let stepList = document.getElementsByClassName('step-list')[0];
+    
     function init() { 
         document.getElementsByClassName("center-container")[0].style.visibility = 'hidden';
         rightContainer.style.display = 'none';
         categoryList();
-        categoryInput.addEventListener('keypress', function(event){ 
-            addCategory(event);
-        });
-        categoryContainer.addEventListener("click", function(event){
-            if ("SPAN" === event.target.tagName) {
-               renderTaskContainer(event);
+        categoryInput.addEventListener('keypress', addCategory);
+        categoryContainer.addEventListener("click", renderTaskContainer);
+        taskInput.addEventListener("keypress", addTask);
+        taskUl.addEventListener("click", renderSubTaskContainer);
+        stepInput.addEventListener("keypress", addStep);
+    }
+
+    /**
+     * This function will add and render new steps for a task
+     * @param event keypress event for step input
+     */
+    function addStep(event) {
+        if (event.key === 'Enter' && stepInput.value != '') {
+            let newStep = {
+                id : subTaskTitle.innerHTML + subTaskId++,
+                name : stepInput.value
             }
-        });
-        taskInput.addEventListener("keypress", function(event) {
-            if (event.key === 'Enter' && taskInput.value != '') {
-                addTask();
-            } 
-        });
-        taskUl.addEventListener("click", function(event) {
-            if (event.target.tagName === 'SPAN') {
-                renderSubTaskContainer(event);
-            } 
-        });
-        stepInput.addEventListener("keypress", function(event) {
-            if (event.key === 'Enter' && stepInput.value != '') {
-                addStep();
-            } 
-        });
+            stepInput.value = '';
+            subTaskList.push(newStep);
+            renderSubTask(newStep);
+        } 
     }
-    function addStep() {console.log(subTaskTitle.innerHTML);
-        let newStep = {
-            id : subTaskTitle.innerHTML + subTaskId++,
-            name : stepInput.value
-        }
-        stepInput.value = '';
-        subTaskList.push(newStep);
-        renderSubTask(newStep);
-    }
+
+    /**
+     * This function will render given subtasks
+     * @param newStep new step object
+     */
     function renderSubTask(newStep) {
         const subTaskList = document.createElement("li");
         const subTaskCheckBox = document.createElement("input");
@@ -99,24 +89,40 @@
         subTaskList.appendChild(document.createElement("hr"));
         stepList.appendChild(subTaskList);
     }
+
+    /** 
+     * This function will render initial catogery list
+     */
     function categoryList() {
         for (let i = 0; i < list.length; i++) {
             renderCategoryList(list[i]);
         }
     }
+    
+    /**
+     * This function will render right container
+     * @param event click event for task list
+     */
     function renderSubTaskContainer(event) {
-        rightContainer.style.display = 'inline-block';
-        centerContainer.style.width = "58%";
-        subTaskTitle.innerHTML = event.target.innerHTML;
-        while (stepList.firstChild) {
-            stepList.removeChild(stepList.firstChild);
-        }
-        for (let i = 0; i < subTaskList.length; i++) {
-            if ((subTaskList[i].id).includes(subTaskTitle.innerHTML)) {
-                renderSubTask(subTaskList[i]);
+        if (event.target.tagName === 'SPAN') {
+            rightContainer.style.display = 'inline-block';
+            centerContainer.style.width = "58%";
+            subTaskTitle.innerHTML = event.target.innerHTML;
+            while (stepList.firstChild) {
+                stepList.removeChild(stepList.firstChild);
+            }
+            for (let i = 0; i < subTaskList.length; i++) {
+                if ((subTaskList[i].id).includes(subTaskTitle.innerHTML)) {
+                    renderSubTask(subTaskList[i]);
+                }
             }
         }
     }
+
+    /** 
+     * This function will render given category
+     * @param value category object
+     */
     function renderCategoryList(value) {
         const category = document.createElement('li');
         const icon = document.createElement('i');
@@ -128,6 +134,11 @@
         category.appendChild(span);
         categoryListBlock.appendChild(category);
     }
+
+    /** 
+     * This function will add and render new category
+     * @param event category input event
+     */
     function addCategory(event) {
         if (event.key == 'Enter') {
             if (categoryInput.value == '') {
@@ -137,38 +148,56 @@
                 id: "category" + cid++,
                 icon: 'fa fa-list',
                 name: categoryInput.value,
-                //taskList : []
             }
             list.push(obj);
             renderCategoryList(obj);
             categoryInput.value = '';
         }  
     }
+
+    /**
+     * This function will render center container
+     * @param event click event for category 
+     */
     function renderTaskContainer(event) {
-        rightContainer.style.display = 'none';
-        centerContainer.style.width = "80%";
-        const taskUl = document.getElementsByClassName('task-ul')[0];
-        categoryId = event.target.id;
-        document.getElementsByClassName("center-container")[0].style.visibility = 'visible';
-        taskTitleblock.innerHTML =  document.getElementById(categoryId).innerHTML;
-        while (taskUl.firstChild) {
-            taskUl.removeChild(taskUl.firstChild);
-        }
-        for (let i = 0; i < taskList.length; i++) {
-            if ((taskList[i].id).includes(categoryId)) {
-                renderTask(taskList[i]);
+        if ("SPAN" === event.target.tagName) {
+            rightContainer.style.display = 'none';
+            centerContainer.style.width = "80%";
+            const taskUl = document.getElementsByClassName('task-ul')[0];
+            categoryId = event.target.id;
+            document.getElementsByClassName("center-container")[0].style.visibility = 'visible';
+            taskTitleblock.innerHTML = document.getElementById(categoryId).innerHTML;
+            while (taskUl.firstChild) {
+                taskUl.removeChild(taskUl.firstChild);
+            }
+            for (let i = 0; i < taskList.length; i++) {
+                if ((taskList[i].id).includes(categoryId)) {
+                    renderTask(taskList[i]);
+                }
             }
         }
     }
-    function addTask() {
-        let newTask = {
-            id : categoryId + "task" + tid++ ,
-            name : taskInput.value
-        }
-        taskInput.value = '';
-        taskList.push(newTask);
-        renderTask(newTask);
+
+    /** 
+     * This function will add and render new task
+     *  @param event keypress event for task input 
+     */
+    function addTask(event) {
+        if (event.key === 'Enter' && taskInput.value != '') {
+            let newTask = {
+                id : categoryId + "task" + tid++ ,
+                name : taskInput.value
+            }
+            taskInput.value = '';
+            taskList.push(newTask);
+            renderTask(newTask);
+        } 
     }
+
+    /** 
+     * This function will render given task
+     * @param newTask new task object
+     */
     function renderTask(newTask) {
         const taskUl = document.getElementsByClassName('task-ul')[0];
         const taskLi = document.createElement("li");
